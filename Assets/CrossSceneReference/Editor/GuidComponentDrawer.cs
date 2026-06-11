f282b85ff2cef04bb7378f9b263060a2df888fd5
Mandariini <timo.moilanen99@outlook.com>
2026-06-11 20:57:37 +0300

    Slight readibility improvement for prefab when using Guid

--- Assets/CrossSceneReference/Editor/GuidComponentDrawer.cs
+++ Assets/CrossSceneReference/Editor/GuidComponentDrawer.cs
@@ -12,8 +12,24 @@ public class GuidComponentDrawer : Editor
         {
             guidComp = (GuidComponent)target;
         }
-       
-        // Draw label
+
+        if (guidComp == null)
+        {
+            return;
+        }
+
+        if (PrefabUtility.IsPartOfPrefabAsset(guidComp.gameObject))
+        {
+            EditorGUILayout.LabelField("Guid:", "<Prefab Asset: intentionally empty>");
+            EditorGUILayout.HelpBox("Prefab assets won't store GUIDs. A unique GUID is generated per scene instance.", MessageType.Info);
+            return;
+        }
+
         EditorGUILayout.LabelField("Guid:", guidComp.GetGuid().ToString());
+
+        if (PrefabUtility.IsPartOfNonAssetPrefabInstance(guidComp.gameObject))
+        {
+            EditorGUILayout.HelpBox("This GUID is instance-specific and stays as a prefab override. Won't be applied.", MessageType.None);
+        }
     }
 }
